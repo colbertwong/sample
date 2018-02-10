439a207fbd04b44cb8d2522499e8f3301aa0b234
colbertwong <colbertwong@foxmail.com>
2018-02-10 16:15:19 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Sample App') - Laraver 入门</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    @yield('content')
+@include('layouts._header')
+<div class="container">
+    <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+    </div>
+</div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.default')
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>主页</h1>
+        <p class="lead">
+            你现在看到的是<a href="">Laravel 入门教程</a>的示例项目主页。
+        </p>
+        <p>
+            一切，将从这里开始。
+        </p>
+        <p>
+            <a href="{{ route('signup') }}" class="btn btn-lg btn-success" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
