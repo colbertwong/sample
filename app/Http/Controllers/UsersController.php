34588eb9c5a3234bd20ff147a5dc4c69858e71d9
colbertwong <colbertwong@foxmail.com>
2018-02-20 12:04:59 +0000

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 
@@ -10,5 +11,28 @@ class UsersController extends Controller
     public function create()
     {
         return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            ]);
+
+        session()->flash('success', '欢迎欢迎！');
+        return redirect()->route('users.show', [$user]);
     }
 }
