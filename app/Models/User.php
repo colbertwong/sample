c0df621b9b95e3e4b7321f0abc91fda7c92f0165
colbertwong <colbertwong@foxmail.com>
2018-03-09 14:38:54 +0000

    Add user following

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Auth;
+use App\Models\Status;
 
 use App\Notifications\ResetPassword;
 
@@ -56,7 +58,11 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this->statuses()->orderBy('created_at', 'desc');
+        $user_ids = Auth::user()->followings->pluck('id')->toArray();
+        array_push($user_ids, Auth::user()->id);
+        return Status::whereIn('user_id', $user_ids)->with('user')->orderBy('created_at', 'desc');
+
+//        return $this->statuses()->orderBy('created_at', 'desc');
     }
 
     public function followers()
@@ -87,6 +93,7 @@ class User extends Authenticatable
 
     public function isFollowing($user_id)
     {
-        return $this->followings()->contains($user_id);
+        return $this->followings->contains($user_id);
+//        return $this->followings()->has($use_id);
     }
 }
--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          * Package Service Providers...
          */
 
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         /*
          * Application Service Providers...
          */
